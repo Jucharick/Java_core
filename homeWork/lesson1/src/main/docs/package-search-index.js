902a8a20ca04cf1ff31fa06cf11fa2432ccc4d17
Julia Ch <juliecha@yandex.ru>
2023-09-09 21:05:15 +0300

    Add docs and dockerfile

---
+++ homeWork/lesson1/src/main/docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.jucharick.lesson1.regular"},{"l":"ru.jucharick.lesson1.sample"}];updateSearchResults();
---
+++ homeWork/lesson1/src/main/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.jucharick.lesson1.regular","l":"Decorator"},{"p":"ru.jucharick.lesson1.sample","l":"Main"},{"p":"ru.jucharick.lesson1.regular","l":"OtherClass"}];updateSearchResults();
