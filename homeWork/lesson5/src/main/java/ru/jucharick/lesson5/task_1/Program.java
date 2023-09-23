191c9267e66a4b76b5df84f1369d4438860992d1
Julia Ch <juliecha@yandex.ru>
2023-09-23 18:14:51 +0300

    Add task_1 - Program backup

---
+++ homeWork/lesson5/src/main/java/ru/jucharick/lesson5/task_1/Program.java
@@ -0,0 +1,15 @@
+package ru.jucharick.lesson5.task_1;
+
+public class Program {
+    /**
+     * Написать функцию, создающую резервную копию всех файлов в директории во вновь созданную папку ./backup
+     */
+
+    public static void main(String[] args) {
+
+    }
+    private static final String BACKUP_PATH = "./backup";
+    private static final String CURRENT_PATH = ".";
+
+    private static
+}
