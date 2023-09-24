89e8bf955e089e269fdb6d2578737c5234074a73
Julia Ch <juliecha@yandex.ru>
2023-09-24 09:25:13 +0300

    UPD Program -backup

--- homeWork/lesson5/src/main/java/ru/jucharick/lesson5/task_1/Program.java
+++ homeWork/lesson5/src/main/java/ru/jucharick/lesson5/task_1/Program.java
@@ -31,6 +31,12 @@ public class Program {
         }
     }
 
+    /**
+     * Метод, копирующий директорию со всеми фалами и поддиректориями в указанную папку
+     * @param fromDir - что скопировать
+     * @param toDir - куда скопировать
+     * @throws IOException
+     */
     public static void copy(String fromDir, String toDir) throws IOException{
         try (DirectoryStream<Path> dir = Files.newDirectoryStream(Path.of(fromDir))) {
             for (Path file : dir) {
@@ -50,11 +56,20 @@ public class Program {
         }
     }
 
+    /**
+     * Метод, копирующий директорию со всеми фалами и поддиректориями в указанную папку
+     * Files.walk returns a stream that is lazily populated with Path by recursively walking the file tree rooted 
+     * at a given starting file. The file tree is traversed depth-first. There are two overloaded Files.walk methods; 
+     * one of them takes the maxDepth parameter, which sets the maximum number of levels of directories to visit.
+     * @param fromDir - что скопировать 
+     * @param toDir - куда скопировать
+     * @throws IOException
+     */
+
     public static void copyDir(String fromDir, String toDir) throws IOException {
         Files.walk(Paths.get(fromDir))
             .forEach(from -> {
-                Path to = Paths.get(toDir, from.toString()
-                        .substring(fromDir.length()));
+                Path to = Paths.get(toDir, from.toString().substring(fromDir.length()));
                 try {
                     Files.copy(from, to);
                 } catch (IOException e) {
